80b163b9bc03640a49aa9763939f977316c749c5
ricardotfs <ricardotfs.oliveira@outlook.com>
2023-04-06 18:25:00 -0300

    Construção do metodo da api register

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {register} = require('../controllers/UserController');
-const {userCreateValidation} = require('../middlewares/useValidation')
-
+const {userCreateValidation} = require('../middlewares/useValidation');
+const validate = require('../middlewares/handleValidation');
 
 //Routes
-router.post('/register',userCreateValidation(), register);
+router.post('/register',userCreateValidation(),validate, register);
 
 module.exports = router;
