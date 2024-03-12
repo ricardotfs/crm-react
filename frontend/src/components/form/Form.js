c4417f5945e876958de7bab7617ef4af50980161
ricardotfs <ricardotfs.oliveira@outlook.com>
2024-03-12 17:03:10 -0300

    ajuste

--- frontend/src/components/form/Form.js
+++ frontend/src/components/form/Form.js
@@ -105,7 +105,7 @@ const Form = () => {
         </h1>
             {loading && <Loding/>}
             {!loading && 
-            <div className="container mt-5">
+            <div className="container">
                 <div className="row">
                     <div className="col-md-3">
                         <div className="nav nav-tabs flex-column nav-pills tabs-container" id="v-pills-tab" role="tablist" aria-orientation="vertical">
--- frontend/src/pages/ticket/Ticket.js
+++ frontend/src/pages/ticket/Ticket.js
@@ -3,10 +3,54 @@ import './Ticket.css'
 //Components
 import Form from '../../components/form/Form'
 
-const Ticket = () =>{
-    return(
+const Ticket = () => {
+    return (
         <div>
-            <Form/>
+            <div id="content-container">
+                <div id="page-content">
+                    <div class='row'>
+                        <div className='col-md-4'>
+                            <div class='row'>
+                                <div className='col-md-12'>
+                                    <h3>Token:TKT0001</h3>
+                                </div>
+                                <div className='col-md-12'>
+                                    <label ><i>*</i> Usuario de cadastro</label>
+                                </div>
+                                <div className='col-md-12 pr-2'>
+                                    <label><i></i>.   . Ricardo Oliveira</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8" id="divTicketDetails">
+                            <div class="row" >
+                                <div className='col-md-4'>
+                                    <label for="campoContato" ><i>*</i> Proprietario</label>
+                                    <select id="campoContato" class="form-control">
+                                        <option value="0">Selecionar</option>
+                                    </select>
+                                </div>
+                                <div className='col-md-4'>
+                                    <label for="campoContato" ><i>*</i> Status</label>
+                                    <select id="campoContato" class="form-control">
+                                        <option value="0">Selecionar</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row" >
+                            <div className='col-md-12'>
+                                    <label for="campoContato" ><i>*</i> Solicitante</label>
+                                    <select id="campoContato" class="form-control">
+                                        <option value="0">Selecionar</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Form />
+                </div>
+            </div>
+
         </div>
     );
 };
