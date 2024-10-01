33abda99cbce58e4929bf5724b6503a665d0d197
ricardotfs <ricardotfs.oliveira@outlook.com>
2024-10-01 17:16:17 -0300

    Construção do novo grid

--- backend/controllers/FormController.js
+++ backend/controllers/FormController.js
@@ -4,12 +4,12 @@ const formServiceDomain = new FormServiceDomain();
 
 const getById = async (req, res) => {
 
-   return FormServiceDomain.formServiceDomain(req, res);
+   return await formServiceDomain.getById(req, res);
 }
 
 const update = async (req, res) => {
 
-    return FormServiceDomain.update(req, res); 
+    return await formServiceDomain.update(req, res); 
 }
 
 module.exports = {
